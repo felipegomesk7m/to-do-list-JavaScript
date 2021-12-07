376d675042d927e9f1e592af34dbc2f4004d62a4
Felipe Gomes <felipegomesk7m@gmail.com>
2021-12-07 17:49:20 -0300

    feat. função de adicionar

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".inputField button");
 
 inputBox.onkeyup = () => {
     let userData = inputBox.value;
@@ -10,3 +11,30 @@ inputBox.onkeyup = () => {
     }
 }
 
+addBtn.onclick = () => {
+    let userData = inputBox.value;
+    let getLocalStorage = localStorage.getItem("Nova Tarefa");
+    if (getLocalStorage == null) {
+        listArr = [];
+    } else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userData);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+}
+
+function showTasks() {
+    let getLocalStorage = localStorage.getItem("Nova Tarefa");
+    let newLiTag = '';
+    if (getLocalStorage == null) {
+        listArr = [];
+    } else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.forEach((element, index) => {
+        newLiTag += `<li> ${element} <span><i class="fas fa-trash"></i></span></li>`
+    });
+    todoList.innerHTML = newLiTag;
+    inputBox.value = '';
+}
